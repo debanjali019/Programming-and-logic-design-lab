c8938e94cac945d381578cab1eed5c019319b809
debanjali019 <singharoydebanjali2004@gmail.com>
2025-03-28 14:29:27 +0530

    Add files via upload

---
+++ Day 5/digits-using-for-loop.cpp
@@ -0,0 +1,14 @@
+//number of digits using for loop
+#include <stdio.h>
+int main()
+{
+	int num, count=0;
+	printf("Enter a number: ");
+	scanf("%d",&num);
+	for(;num!=0; num/=10)
+	{
+		count ++;
+	}
+	printf("The number of digits given in the input is : %d", count);
+	return 0;
+}
